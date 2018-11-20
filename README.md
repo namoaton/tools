be055d66f1d8b708746d5d094002a327a0388295
maxim.blashchuk <namoaton@gmail.com>
2018-11-20 08:52:54 +0200

    Add five_percent_tax.py

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # tools
 *modules_update.py* - update all installed python3 modules using pip3
+*five_percent.py* - get exchange course from National Bank of Ukraine and calculate 5% from total sum in UAH
+>python3 five_percent_tax.py YYYYMMDD:SUM_IN_USD YYYYMMDD:SUM_IN_USD ....
---
+++ five_percent_tax.py
@@ -0,0 +1,48 @@
+import urllib.request
+import requests
+import json
+import sys
+URL = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?date=XXXXXXXX&json"
+
+
+def get_currency_rate(date):
+    r = requests.get(URL.replace('XXXXXXXX', date))
+    data = json.loads(r.content.decode())
+    for currency in data:
+        if currency['cc'] == 'USD':
+            print(currency['exchangedate'], currency['rate'])
+            return currency['rate']
+
+
+def get_money_for_date(date, money):
+    currency = get_currency_rate(date)
+    return currency * money
+
+
+def get_5_percent(values):
+    total_sum = 0
+    for val in values:
+        total_sum = total_sum + val
+    print("Total sum =",total_sum)
+    return total_sum * 0.05
+
+
+def get_data(argv):
+    data = argv[1:]
+    data_dic = {}
+    for val in data:
+        val_list = val.split(':')
+        data_dic[val_list[0]] = int(val_list[1])
+    print(data_dic)
+    return (data_dic)
+
+
+def processing(data_dic):
+    money = []
+    for key in data_dic:
+        money.append(get_money_for_date(key, data_dic[key]))
+    print("5 percent = ", get_5_percent(money))
+
+
+if __name__ == "__main__":
+    processing(get_data(sys.argv))
